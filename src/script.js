f420c12bc84ed335d185eb78bbeefeab90356773
molinarijess <lacerdajess@gmail.com>
2022-01-11 15:33:18 +0000

    reviewing prompt and alert js

---
+++ src/script.js
@@ -0,0 +1,6 @@
+let hometown = prompt("Where do you live?");
+alert(`You live in ${hometown}`);
+
+let firstName = prompt("What's your first name?");
+let lastName = prompt(`What about your last name, ${firstName}?`);
+alert(`Nice to know you ${firstName} ${lastName}`);
