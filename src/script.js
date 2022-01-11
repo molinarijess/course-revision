55a4133c41ea49538396967c8c77fd29e8cb1c4b
molinarijess <lacerdajess@gmail.com>
2022-01-11 17:01:42 +0000

    js reviewing function creation

--- src/script.js
+++ src/script.js
@@ -1,10 +1,14 @@
-let country = prompt("What country are you from?");
-country = country.toLowerCase();
-country = country.trim();
-country = country.replace("brasil", "brazil");
+function canSpeakPortuguese() {
+  let country = prompt("What country are you from?");
+  country = country.toLowerCase();
+  country = country.trim();
+  country = country.replace("brasil", "brazil");
 
-if (country === "portugal" || country === "brazil") {
-  alert("You speak Portuguese");
-} else {
-  alert("You don't speak Portuguese");
+  if (country === "portugal" || country === "brazil") {
+    alert("You speak Portuguese");
+  } else {
+    alert("You don't speak Portuguese");
+  }
 }
+
+canSpeakPortuguese();
